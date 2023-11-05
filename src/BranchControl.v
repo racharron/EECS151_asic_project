3574fd20cfe3aa727187c2ea3030a19721da809a
Rawley Charron <rawleycharron@gmail.com>
2023-11-05 14:00:59 -0800

    Implemented a branch control module

---
+++ src/BranchControl.v
@@ -0,0 +1,16 @@
+module BranchControl (
+    input [31:0] A, B,
+    input funct3,
+    output branch,
+);
+    wire less, unsign, inv, less_than, br_equal;
+    assign {less, unsign, inv} = funct3;
+    assign branch = inv ^ (less ? less_than : br_equal);
+    branch_comp bc (
+        .unsign(unsign),
+        .A(A),
+        .B(B),
+        .less_than(less_than),
+        .br_equal(br_equal)
+    );
+endmodule
