a5ac63919957f1aed69266b5953f0c560dd98323
Rawley Charron <rawleycharron@gmail.com>
2023-11-09 10:45:34 -0800

    Finished Writeback implementation

--- src/Writeback.v
+++ src/Writeback.v
@@ -1,44 +1,40 @@
 module Writeback (
-  input clk,
+  input clk, reset, stall, mem_ready,
   input [31:0] pc,
   input [31:0] alu_result,
   input [31:0] dcache_output,
   input [2:0] funct3,
-  input reg_we, mem_we, mem_rr, csr_write, jump,
+  input reg_we, mem_we, mem_rr, jump,
 
-  output [31:0] writeback, addr,
+  output [31:0] writeback, addr
   /// Tells the CPU to pause for at least one cycle, so that the memory has time to at least get the address.
   /// In the no_cache_mem model, there is no stall signal to tell the CPU to wait a cycle.
-  output pause
+  output initial_pause
 );
   
   
-  reg pause_bit;
+  reg continue_load;
+  wire jalr;
   wire [31:0] masked_load;
 
   ld mask (
     .mem_address(alu_result),
-    .mem_output(unmasked_load),
+    .mem_output(dcache_output),
     .funct3(funct3),
     .load_out(masked_load)
   );
   
-  wire [31:0] pc_plus_four;
-  assign pc_plus_four = pc + 32'd4;
 
-  always @(*) begin
-    reg_write_enable = 1'b1;
-    writeback = alu_result;
-        
-    //writeback mux
-    if (is_store) reg_write_enable = 1'b0;
-    else if (is_load) writeback = masked_load;
-    else if (is_branch) reg_write_enable = 1'b0;
-    else if (is_jump) writeback = pc_plus_four;
+  assign initial_pause = mem_rr & !continue_load;
+  assign addr = alu_result;
+  assign jalr = reg_we & jump;
+  assign writeback = jalr ? pc + 32'd4
+    : mem_rr ? masked_load
+    : alu_result;
 
-    if (csr_enable) begin
-        reg_write_enable = 1'b0;
-    end 
-    unmasked_load = dcache_output;
-  end 
+  always @(posedge clk) begin
+    if (reset) continue_load <= 1'b0;
+    else if (continue_load & mem_ready & !stall) continue_load <= 1'b0;
+    else if (mem_rr) continue_load <= 1'b1;
+  end
 endmodule
